f411a46c2fb325de06343716e136566987be0b6c
realjema <realjema@gmail.com>
2021-05-27 14:09:49 +0100

    study area content ui update

--- src/views/Study.js
+++ src/views/Study.js
@@ -45,7 +45,7 @@ const Study = (props) => {
 		<>
 			{/* Page content */}
 
-			<Container className="col-lg-12 sidebar">
+			<Container className="col-lg-12 no-padding">
 				<Sidebar
 					sidebar={<SectionSidebar parts={parts} />}
 					open={sidenav}
@@ -77,7 +77,7 @@ const Study = (props) => {
 							left: 0,
 							right: 0,
 							bottom: 0,
-							overflowY: 'auto',
+							overflowY: 'unset',
 							WebkitOverflowScrolling: 'touch',
 							transition: 'left .3s ease-out, right .3s ease-out',
 						},
@@ -101,42 +101,40 @@ const Study = (props) => {
 						},
 					}}
 				>
-					<Container className="col-xs-10 col-lg-6">
-						<Row>
-							<button onClick={openSideNav}>Open sidebar</button>
-
-							<div className="content">
-								<h3>Main content</h3>
-								<br />
+						<Row className="studyHeader">
+							<Col lg={9} className="studyHeaderContents col-10">
 								<p>
-									Nam accumsan eleifend metus at imperdiet. Mauris pellentesque ipsum nisi, et
-									fringilla leo blandit sed. In tempor, leo sit amet fringilla imperdiet, ipsum enim
-									sagittis sem, non molestie nisi purus consequat sapien. Proin at velit id elit
-									tincidunt iaculis ac ac libero. Vivamus vitae tincidunt ex. Duis sit amet lacinia
-									massa. Quisque lobortis tincidunt metus ut commodo. Sed euismod quam gravida
-									condimentum commodo.
+									<Button outline color="primary" type="button" onClick={openSideNav}>
+										<span className="btn-inner--icon">
+											<i className="ni ni-bullet-list-67"></i>
+										</span>
+									</Button>
+									<span>Practice home / Introduction / Random</span>
 								</p>
-								<br />
-								<p>
-									Vivamus tincidunt risus ut sapien tincidunt, ac fermentum libero dapibus. Duis
-									accumsan enim ac magna tempor, vestibulum euismod nisl pharetra. Ut dictum lacus eu
-									venenatis vestibulum. Vestibulum euismod at arcu ac blandit. Curabitur eu imperdiet
-									magna. Duis bibendum efficitur diam, eget placerat nunc imperdiet eget. Morbi porta
-									at leo sed porta. Nullam eleifend eleifend quam eget dictum.
-								</p>
-								<br />
-								<p>
-									Sed nulla erat, lacinia sit amet dui at, cursus blandit neque. In ultricies, dui a
-									laoreet dignissim, risus mi cursus risus, at luctus sem arcu non tortor. In hac
-									habitasse platea dictumst. Etiam ut vulputate augue. Aenean efficitur commodo ipsum,
-									in aliquet arcu blandit non. Praesent sed tempus dui, non eleifend nisi. Proin non
-									finibus diam, quis finibus ante. Fusce aliquam faucibus mauris, id consequat velit
-									ultricies at. Aliquam neque erat, fermentum non aliquam id, mattis nec justo. Nullam
-									eget suscipit lectus.
-								</p>
-							</div>
+								<h1 className="display-1 mb-0">{title}</h1>
+								<small> 5 Parts | Multiple Choice </small>
+							</Col>
 						</Row>
-					</Container>
+						<Row className="justify-content-lg-left">
+							<Col lg={6} className="studyHeaderContents col-10">
+								<div>
+									<h2 className="display-3">{part.title}</h2>
+									<p className="studyContent">{part.content}</p>
+								</div>
+							</Col>
+						</Row>
+						<Row className="justify-content-lg-center col-lg-6 studyHeaderContents">
+							<Col className="text-left" xs="6">
+								<Button color="primary" onClick={handleClickBack} size="md">
+									Prev
+								</Button>
+							</Col>
+							<Col className="text-right" xs="6">
+								<Button color="primary" size="md" onClick={handleClick}>
+									Next
+								</Button>
+							</Col>
+						</Row>
 				</Sidebar>
 			</Container>
 		</>
